49c9232f1825d5b35090a061d3d5d8e81958ad23
Milky435 <xaviersilver2006@gmail.com>
2024-01-13 19:39:58 +1100

    added quit button and other

--- number_guesser.py
+++ number_guesser.py
@@ -1,17 +1,42 @@
+print("type quit to save and leave")
 # setup
+
+import sys
+import time
 import random
 from random import randrange
+# streak
+streak = 0
+# loop
+
 looper = True
 while looper == True:
- 
+ print("you have a streak of", streak)
  num = randrange(10)
  num2 = randrange(10)
  correct = num + num2
- 
+ # guessing
+
+ is_int = False
  print(num, "+", num2, "=")
- guess = int(input("type answer here: "))
- 
- if guess == correct:
+ guess = input("type answer here: ")
+ # see if guess is correct
+ try:
+    guess_int = int(guess)
+    is_int = True
+ except:
+    is_int = False
+ if is_int:
+
+  if guess == correct:
      print("correct")
- else:
+     streak += 1
+     time.sleep(1)
+  elif guess == str(quit):
+    sys.exit()
+  else:
      print("wrong")
+     streak = 0
+     time.sleep(1)
+ elif guess == "quit":
+   sys.exit()
